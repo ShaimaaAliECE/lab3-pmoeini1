5061468b4d70c134ff0cb3a91e413d039e083aeb
pmoeini1 <pmoeini@uwo.ca>
2021-11-18 09:11:58 -0500

    Start dynamic handling

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ const app = express();
 // serve static contents
 app.use(express.static('static'))
 // dynamic handling
+app.get('/admin', (req, res) => {
+    let content = '';
+    res.send(content);
+})
+app.get('/guest', (req, res) => {
+    let content = '';
+    res.send(content);
+})
 
 app.listen(80);
