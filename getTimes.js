aeeba27e93e1df87f3d186a6244086ed43972589
pmoeini1 <pmoeini@uwo.ca>
2021-11-18 15:42:26 -0500

    Allow user to see previous submission

---
+++ getTimes.js
@@ -0,0 +1,51 @@
+export default function getTimes() {
+    let conn = mysql.createConnection({
+        host: '34.72.236.12',
+        user: 'root',
+        password: 'password',
+        database: 'MyStoreDB',
+    });
+    
+    conn.connect();
+
+    var adminTimes = '';
+    adminTimes += "<h1>Times Selected By Admin</h1>";
+    adminTimes += "<br />";
+    adminTimes += "<br />";
+
+    conn.query(
+        'SELECT * FROM AdminTimes',
+        (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+            }
+            for (r in rows) {
+                adminTimes += "<h4>" + r.AvailableTime + "</h4>";
+                adminTimes += "<br />";
+            }
+        }
+    );
+
+    var guestTimes = '';
+    guestTimes += "<br />"
+    guestTimes += "<h1>Times Selected By Guests</h1>";
+    guestTimes += "<br />";
+    guestTimes += "<br />";
+
+    conn.query(
+        'SELECT * FROM GuestAvailabilities',
+        (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+            }
+            for (r in rows) {
+                guestTimes += "<h4>" + r.GuestName + ": " + r.AvailableTime + "</h4>";
+                guestTimes += "<br />"
+            }
+        }
+    );
+
+    guestTimes += "<br />"
+
+    return adminTimes + guestTimes;
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+import getTimes from './getTimes';
 
 const app = express();
 
@@ -7,13 +8,13 @@ app.use(express.static('static'))
 // dynamic handling
 app.get('/admin', (req, res) => {
     res.redirect("admin.html");
-})
+});
 app.get('/guest', (req, res) => {
     res.redirect("guest.html");
-})
+});
 app.get('/previous', (req, res) => {
-    let content = '';
+    let content = getTimes();
     // attach all previous submissions
     res.send(content);
-})
+});
 app.listen(80);
